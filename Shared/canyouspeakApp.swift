509b811c3edac2b983bff2eb4ccf51fe40a5679c
tripthyme <huseinkareem@aol.com>
2022-06-18 12:35:15 -0400

    initial commit

---
+++ Shared/canyouspeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  canyouspeakApp.swift
+//  Shared
+//
+//  Created by Husein Kareem on 6/18/22.
+//
+
+import SwiftUI
+
+@main
+struct canyouspeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
